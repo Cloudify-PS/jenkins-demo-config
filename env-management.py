b9d00c1aa8e69b011b95df85b0299e47e60e7f9c
Isaac Shabtay <isaac@shabtay.com>
2019-11-24 01:43:05 -0800

    WIP

--- env-management.py
+++ env-management.py
@@ -75,23 +75,43 @@ def _get_rest_client(managers, manager_id):
     return CloudifyClient(**manager_desc)
 
 
-def create(managers, blueprint_id, deployment_id, inputs_file, outputs_file, **kwargs):
-    manager_id = managers['topologies'][blueprint_id]
-    client = _get_rest_client(managers, manager_id)
-    with open(inputs_file, 'r') as f:
-        inputs = yaml.safe_load(f)
+def _create_deployment(client, blueprint_id, deployment_id, inputs):
     client.deployments.create(
         blueprint_id=blueprint_id,
         deployment_id=deployment_id,
         inputs=inputs)
-    # Wait for deployment to finish (needed as per CYBL-955).
+    # Wait for deployment to finish (CYBL-955 would save this REST call).
     dep_execution = client.executions.list(deployment_id=deployment_id)[0]
     follow_execution(client, dep_execution)
+
+
+def _delete_deployment(client, deployment_id):
+    client.deployments.delete(deployment_id=deployment_id)
+
+
+def _install(client, deployment_id):
     install_execution = client.executions.start(
         deployment_id=deployment_id,
         workflow_id='install'
     )
     follow_execution(client, install_execution)
+
+
+def _uninstall(client, deployment_id):
+    uninstall_execution = client.executions.start(
+        deployment_id=deployment_id,
+        workflow_id='uninstall'
+    )
+    follow_execution(client, uninstall_execution)
+
+
+def create(managers, blueprint_id, deployment_id, inputs_file, outputs_file, **kwargs):
+    manager_id = managers['topologies'][blueprint_id]
+    client = _get_rest_client(managers, manager_id)
+    with open(inputs_file, 'r') as f:
+        inputs = yaml.safe_load(f)
+    _create_deployment(client, blueprint_id, deployment_id, inputs)
+    _install(client, deployment_id)
     capabilities = client.deployments.capabilities.get(deployment_id)
     outputs = client.deployments.outputs.get(deployment_id)
     with open(outputs_file, 'w') as f:
@@ -105,13 +125,27 @@ def create(managers, blueprint_id, deployment_id, inputs_file, outputs_file, **k
 
 def delete(managers, manager_id, deployment_id, **kwargs):
     client = _get_rest_client(managers, manager_id)
-    uninstall_execution = client.executions.start(
-        deployment_id=deployment_id,
-        workflow_id='uninstall'
+    _uninstall(client, deployment_id)
+    _delete_deployment(client, deployment_id)
+
+
+def install(managers, manager_id, app_blueprint_path, app_id, inputs_file, **kwargs):
+    client = _get_rest_client(managers, manager_id)
+    client.blueprints.upload(
+        path=app_blueprint_path,
+        entity_id=app_id
     )
-    uninstall_execution = follow_execution(client, uninstall_execution)
-    follow_execution(client, uninstall_execution)
-    client.deployments.delete(deployment_id=deployment_id)
+    with open(inputs_file, 'r') as f:
+        inputs = json.load(f)
+    _create_deployment(client, app_id, app_id, inputs)
+    _install(client, app_id)
+
+
+def uninstall(managers, manager_id, app_id, inputs, **kwargs):
+    client = _get_rest_client(managers, manager_id)
+    _uninstall(client, app_id)
+    _delete_deployment(client, app_id)
+    client.blueprints.delete(app_id)
 
 
 def main():
@@ -128,6 +162,15 @@ def main():
     delete_subparser = subparsers.add_parser('delete', parents=[common_parser])
     delete_subparser.add_argument('--manager-id', metavar='ID', required=True)
     delete_subparser.set_defaults(func=delete)
+    install_subparser = subparsers.add_parser('install', parents=[common_parser])
+    install_subparser.add_argument('--app-blueprint', dest='app_blueprint_path', metavar='FILE', required=True)
+    install_subparser.add_argument('--id', dest='app_id', metavar='ID', required=True)
+    install_subparser.add_argument('-i', '--inputs', dest='inputs_file', metavar='FILE', required=True)
+    install_subparser.add_argument('--manager-id', metavar='ID', required=True)
+    install_subparser.set_defaults(func=install)
+    uninstall_subparser = subparsers.add_parser('uninstall', parents=[common_parser])
+    uninstall_subparser.add_argument('--manager-id', metavar='ID', required=True)
+    uninstall_subparser.set_defaults(func=uninstall)
 
     with open(os.path.join(THIS_DIR, 'managers.yaml')) as f:
         managers = yaml.safe_load(f)
